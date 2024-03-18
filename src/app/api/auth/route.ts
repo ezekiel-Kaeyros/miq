2f26758f10e0905141aa7355071b1661f1fe9765
ezekiel-kaeyros <ezekiel.dombissi@kaeyros-analytics.com>
2024-03-18 17:52:46 +0100

    first

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,28 +1,2 @@
-import { NextApiRequest } from 'next';
-import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../lib/dbConnect';
-import User from '../models/user'
-import { NextResponse } from "next/server";
-
-export async function POST(request: any) {
-  let { password, email} = await request.json();
-  await dbConnect();
-
-  try {
-    // Get the user with the provided email
-    const user= await User.find({email: email})
-    if (user.length > 0) {
-      const passwordMatches = await bcrypt.compare(password, user[0].password);
-
-      if (passwordMatches) {
-        return NextResponse.json({status: 'Success', message: 'Login successful'}, { status: 201 });
-      } else {
-        return NextResponse.json({ status: 'Error', message: 'Invalid password' }, { status: 403 });
-      }
-    } else {
-      return NextResponse.json({ status: 'Error', message: 'User not found' }, { status: 401 });
-    }
-  } catch (error) {
-    return NextResponse.json({ status: 'Error', message: 'something wrong' }, { status: 500});
-  }
-}
